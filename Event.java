fbb10fc44bcba1d1cf4cc15e27d3e722c3fd6ec1
Kacper <kacperkozbial@onet.pl>
2017-10-31 15:29:03 +0100

    Update Event.java

--- Event.java
+++ Event.java
@@ -2,8 +2,22 @@ command /event [<text>]:
 	trigger:
 		if arg 1 is "dolacz" or "join":
 			if {eventhalloween::status::%player%} is "&aUdzial":
-				add "%player%" to {event::list::*}
-				send coloured "&7Dolacyzles do &3eventu" to the player
+				if {event::list::*} doesn't contains "%player%":
+					add "%player%" to {event::list::*}
+					send coloured "&7Dolaczyles do &3eventu" to the player
 			else:
 				send "&cWpisz /dynia aby wziac udzial!" to the player
 		stop
+
+every 10 minutes:
+	loop {event::list::*}:
+		add 1 to {_x}
+		set {_x2} to a random integer between 1 and size of {event::list::*}
+		if {_x} = {_x2}:
+			set {_winner} to loop-index
+			exit loop
+			set {_item} to a random item out of all items
+			broadcast "&6%{_winner}% &7wygral &3%{_item}%"
+			wait 3 seconds
+			add "%{_item}%" to the {_winner}'s inventory
+	clear {event::list::*}
